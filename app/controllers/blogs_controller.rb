16bf15fe98bf93ea152a6b95497f1778efcf1bb5
parthsoni262 <parthsoni694@gmail.com>
2021-01-08 11:30:32 +0530

    Integrated helper methods from ActionView

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -29,9 +29,9 @@ class BlogsController < ApplicationController
   # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
-
+    #@user.books.save
     respond_to do |format|
-      if @blog.save
+      if @blog.blogs.save
         format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
       else
         format.html { render :new }
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -21,4 +21,7 @@ class Portfolio < ApplicationRecord
   	self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
   	self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
   end
+
+
+  #belongs_to :topic
 end
